8df166a694e366e209d9d202127962f624b81423
Ahn <81910935+BYEONGJIN-AHN@users.noreply.github.com>
2022-04-04 13:16:01 +0900

    Feat #41 Main Page Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Main from './pages/main';
+
 function App(): JSX.Element {
-  return <div>Hello!</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
