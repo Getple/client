7e3bd8f8397a47f50a5e97eb8292708d26417e0c
Ahn <81910935+BYEONGJIN-AHN@users.noreply.github.com>
2022-04-04 14:47:43 +0900

    Design #6 Constant Settings

---
+++ src/constant/fontStyle.ts
@@ -0,0 +1,11 @@
+export default class Font {
+    static HEAD_1 = { 'font-size': '26px', 'font-weight': '600' };
+    static HEAD_2 = { 'font-size': '20px', 'font-weight': '600' };
+    static HEAD_3 = { 'font-size': '12px', 'font-weight': '600' };
+    static SUBTITLE_1 = { 'font-size': '20px', 'font-weight': '500' };
+    static SUBTITLE_2 = { 'font-size': '16px', 'font-weight': '500' };
+    static SUBTITLE_3 = { 'font-size': '14px', 'font-weight': '500' };
+    static BODY_1 = { 'font-size': '16px', 'font-weight': '400' };
+    static BODY_2 = { 'font-size': '14px', 'font-weight': '400' };
+    static BODY_3 = { 'font-size': '12px', 'font-weight': '400' };
+}
