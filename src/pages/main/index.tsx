eb8284c0fec5582b7faba0d4b5a28963df0c9887
Ahn <81910935+BYEONGJIN-AHN@users.noreply.github.com>
2022-04-04 14:50:04 +0900

    Chore #6 fontAwesome 패키지 설치

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,7 +1,12 @@
+import styled from 'styled-components';
+import { faCamera } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 const Main = (): JSX.Element => {
   return (
     <>
       <div>Main page</div>
+      <FontAwesomeIcon icon={faCamera} />
     </>
   );
 };
