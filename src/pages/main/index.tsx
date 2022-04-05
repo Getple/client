8e9f06dbbac6ed4a75be8697e600eb8f7156fc72
Ahn <81910935+BYEONGJIN-AHN@users.noreply.github.com>
2022-04-05 20:42:37 +0900

    Design #35 메인페이지 전체적인 틀 마크업 작업

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,14 +1,22 @@
 import Carrousel from '../../components/carrousel';
 import { dummyData1 } from '../../components/carrousel/dummyData/dummyData1';
 import Search from '../../components/search';
+import styled from 'styled-components';
 
 const Main = (): JSX.Element => {
   return (
-    <>
+    <Container>
       <Search />
       <Carrousel array={dummyData1} />
-    </>
+    </Container>
   );
 };
 
+const Container = styled.div`
+  max-width: 1120px;
+  height: calc(100vh - 300px - 64px);
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+`;
 export default Main;
