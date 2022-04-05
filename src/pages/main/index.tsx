32bac18760931e750fba0de508dd64330e948848
jisoo <devjisoolee@gmail.com>
2022-04-05 15:18:33 +0900

    Chore #21 Merge conflict

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,9 +1,6 @@
 import Search from '../../components/search';
-<<<<<<< HEAD
-=======
 import { faCamera } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
->>>>>>> dd5d68df7c83a9d09ffeb4a3732d14b6d5727c97
 
 const Main = (): JSX.Element => {
   return (
