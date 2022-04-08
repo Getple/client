e905c6f11ff1487000c3485a4c0003eb053dbc6b
y-solb <ysolb99@gmail.com>
2022-04-09 00:58:25 +0900

    Feat #78 submit post

--- src/pages/postForm/postFormContainer.tsx
+++ src/pages/postForm/postFormContainer.tsx
@@ -1,7 +1,14 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import PostFormPresenter from './postFormPresenter';
 
 const PostFormContainer = () => {
+  const titleRef = useRef<HTMLInputElement>(null);
+  const countRef = useRef<HTMLInputElement>(null);
+  const phoneRef = useRef<HTMLInputElement>(null);
+  const sportRef = useRef<HTMLInputElement>(null);
+  const locationRef = useRef<HTMLInputElement>(null);
+  const contentRef = useRef<HTMLTextAreaElement>(null);
+
   const [isShow, setIsShow] = useState({ sport: false, location: false });
   const [selectedSport, setSeletctedSport] = useState<string>('');
   const [selectedLocation, setSeletctedLocation] = useState<string>('');
@@ -24,6 +31,19 @@ const PostFormContainer = () => {
     }
   };
 
+  const handleSubmit = () => {
+    const post = {
+      id: Date.now(),
+      title: titleRef.current?.value,
+      count: countRef.current?.value,
+      phone: phoneRef.current?.value,
+      sport: sportRef.current?.value,
+      location: locationRef.current?.value,
+      content: contentRef.current?.value,
+    };
+    console.log(post);
+  };
+
   return (
     <PostFormPresenter
       setSeletctedSport={setSeletctedSport}
@@ -32,6 +52,13 @@ const PostFormContainer = () => {
       selectedLocation={selectedLocation}
       selectedSport={selectedSport}
       handleDropdown={handleDropdown}
+      titleRef={titleRef}
+      countRef={countRef}
+      phoneRef={phoneRef}
+      sportRef={sportRef}
+      locationRef={locationRef}
+      contentRef={contentRef}
+      handleSubmit={handleSubmit}
     />
   );
 };
